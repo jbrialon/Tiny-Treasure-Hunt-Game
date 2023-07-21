cb24d146882081b84071758b99b0408f2e2db1cb
Jérémy Brialon <jeremzor@gmail.com>
2023-07-21 12:01:35 +0200

    initial commit Unity Project Files

---
+++ Assets/Scripts/rotateObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ySpeed = -5f * Time.deltaTime;
+        transform.Rotate(0, ySpeed, 0);
+    }
+}
